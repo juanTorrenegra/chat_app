c889d433841f66f21096412794db534571aa18eb
juan <juantorrenegra2000@gmail.com>
2025-06-23 23:29:40 -0500

    296 sending chat msg+data to firestore

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -8,7 +10,7 @@ class NewMessage extends StatefulWidget {
 } //294
 
 class _NewMessageState extends State<NewMessage> {
-  var _messageController = TextEditingController();
+  final _messageController = TextEditingController();
 
   @override
   void dispose() {
@@ -16,15 +18,29 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   } //294
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
-    //send to firebase
 
+    FocusScope.of(context).unfocus(); //removes keyboard
     _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!; //296
+    final userData = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user.uid)
+        .get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    }); //296 send to firebase
   } //294
 
   @override

c8a884388271fc745cf9816d30988284e2c3b169
juan <juantorrenegra2000@gmail.com>
2025-06-23 23:01:47 -0500

    294 adding chat messages and input widget

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('no messages found'));
+  }
+}//294
